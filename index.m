a2d684ae0e4c8c178e189ec872721df9a3047a2f
Alick_lazare97 <alick_lazare97@live.com>
2023-12-16 14:06:01 -0500

    formatting

--- index.m
+++ index.m
@@ -4,39 +4,31 @@ scriptPath = fileparts(mfilename('fullpath'));
 imageFolder = fullfile(scriptPath, '/resources/images');
 
 % Get a list of all files in the folder
-imageFiles = dir(fullfile(imageFolder, '*.png')); % Change '*.jpg' to the appropriate format if needed
+imageFiles = dir(fullfile(imageFolder, '*.png'));
 
-% Initialize a cell array to store the extracted text
 licensePlateTexts = {};
 
-% Loop over each image file
 for i = 1:length(imageFiles)
-    % Full path to image
     imagePath = fullfile(imageFolder, imageFiles(i).name);
     
-    % Read the image
     img = imread(imagePath);
-    figure; % Create a new figure window
-    imshow(img); % Show the image
-    h = drawrectangle; % Let the user draw a rectangle
-    roi = round(h.Position); % Get the position of the rectangle
-    %roi = round(getPosition(drawrectangle))
-    % Perform OCR
-    ocrRes = ocr(img, roi);
-    
-    % Store the recognized text
-    licensePlateTexts{end+1} = ocrRes.Text;
+    figure; 
+    imshow(img); 
+    h = drawrectangle; %draw rectangle around area with text
+    roi = round(h.Position); %generate region of interest from rectangle
+    ocrRes = ocr(img, roi); %perform ocr on region of interest
+    licensePlateTexts{end+1} = ocrRes.Text; %extract text from ocr result
 
-       % Display the image
-    figure; % Create a new figure window
+    %display results
+    figure; 
     Iocr = insertText(img,roi(1:2),ocrRes.Text,AnchorPoint="RightTop",FontSize=16);
     imshow(Iocr)
-    title(['License Plate for Image ', ocrRes.Text]); % Title with image number
+    title(['License Plate for Image ', ocrRes.Text]); % Title with license plate text
     
-    % Optionally, you can display each plate's text
+    % display result in command 1 by one
     fprintf('License Plate Text for Image %d: %s\n', i, ocrRes.Text);
 end
 
-% Display all extracted texts
+% Display all extracted license plate numbers
 disp('All Extracted License Plate Texts:');
 disp(licensePlateTexts);
